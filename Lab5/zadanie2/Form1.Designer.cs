2949b1e08bd04c5288a0d4d48bfc250f92c7ee35
kubix23 <kubix2770@gmail.com>
2024-05-15 09:18:36 +0200

    Add exercice 2

---
+++ Lab5/zadanie2/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace zadanie2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.resultLabel.Location = new System.Drawing.Point(32, 34);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(105, 29);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.Text = "[WYNIK]";
+            this.resultLabel.Click += new System.EventHandler(this.resultLabel_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(35, 147);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(170, 38);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(37, 89);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(389, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Compress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Decompress";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 286);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.resultLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab5/zadanie2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie2
+{
+    public partial class Form1 : Form
+    {
+        private int numberToCompute = 0;
+        private FolderBrowserDialog folderBrowserDialog1;
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private BigInteger fibo(int n, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            BigInteger result = BigInteger.Parse("1");
+            BigInteger a = BigInteger.Parse("0");
+            BigInteger b = BigInteger.Parse("1");
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                for (int i = n; i > 0; i--)
+                {
+                    int percentComplete = (int)(((n - i * 1.0 + 1) / n) * 100);
+                    worker.ReportProgress(percentComplete);
+                    Thread.Sleep(30);
+                    result = a + b;
+                    if (i % 2 == 0) { b = result; } else { a = result; }
+                }
+            }
+            return result;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            e.Result = fibo((int)e.Argument, worker, e);
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                resultLabel.Text = "Canceled";
+            }
+            else
+            {
+                resultLabel.Text = e.Result.ToString();
+            }
+            startButton.Enabled = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+            startButton.Enabled = false;
+            numberToCompute = 99990;
+            backgroundWorker1.RunWorkerAsync(numberToCompute);
+        }
+
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog() { Description = "Select directory to open" };
+            dlg.ShowDialog();
+            DirectoryInfo temp = new DirectoryInfo(dlg.SelectedPath);
+            FileInfo[] files = temp.GetFiles();
+            Parallel.ForEach(files, (i) =>
+            {
+                FileStream originalFileStream = File.Open(i.FullName, FileMode.Open);
+                FileStream compressedFileStream = File.Create($"{i.Name}.gz");
+                var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
+                originalFileStream.CopyTo(compressor);
+                compressor.Dispose();
+                compressedFileStream.Dispose();
+                originalFileStream.Dispose();
+            });
+            dlg.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            DirectoryInfo temp = new DirectoryInfo(dlg.SafeFileName);
+            FileStream compressedFileStream = File.Open(temp.FullName, FileMode.Open);
+            FileStream outputFileStream = File.Create(temp.Name.Split('.')[0] + '.' + temp.Name.Split('.')[1]);
+            var decompressor = new GZipStream(compressedFileStream, CompressionMode.Decompress);
+            decompressor.CopyTo(outputFileStream);
+            decompressor.Dispose();
+            compressedFileStream.Dispose();
+            outputFileStream.Dispose();
+            dlg.Dispose();
+        }
+    }
+}
